726a4efe1432c279b0530bab4519cc7d1c95f37d
hjStack <hye_jun0209@icloud.com>
2024-07-08 17:35:35 +0900

    테스트 코드 추가

--- src/test/java/SpringBasic/basic/MemberServiceTest.java
+++ src/test/java/SpringBasic/basic/MemberServiceTest.java
@@ -1,4 +1,31 @@
 package SpringBasic.basic;
 
+import SpringBasic.basic.member.Member;
+import SpringBasic.basic.member.MemberService;
+import SpringBasic.basic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MemberServiceTest {
+
+    MemberService memberService=new MemberServiceImpl() ;
+
+    @Test
+     void join(){
+        //given
+        Member member=new Member(Grade.VIP,1L,"memberA");  //1L : 1 Long
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findByMember(1L);
+
+        //then  //검증은 = assertions.assertThat().isEqualTo();
+        
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+
+
 }
